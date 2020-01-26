8d97af697c890386fdcbbf16ab9023b430afa8bf
Ben Chatelain <ben@octop.ad>
2020-01-25 22:18:51 -0700

    ✨ Finish mountEFI script

--- mountEFI.sh
+++ mountEFI.sh
@@ -10,8 +10,19 @@ if test -d $mount_point; then
     exit 1
 fi
 
-# diskutil list
+diskutil list
+
+echo "Enter integer number of disk containing the EFI partition to mount."
+echo "Example: /dev/disk13s1 -> 13"
+read -r disk_number
+
+if ! test "$disk_number" -gt 0; then
+    echo "Value $disk_number is not a number."
+    exit 2
+fi
 
 echo "Mounting $mount_point, admin password required"
 sudo mkdir $mount_point
-# sudo mount -t msdos /dev/disk13s1 $mount_point
+
+echo "/dev/disk${disk_number}s1"
+sudo mount -t msdos "/dev/disk${disk_number}s1" $mount_point
